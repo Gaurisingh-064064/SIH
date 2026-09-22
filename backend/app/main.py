021b67b92338aeb82b488529450fc992d3a47f95
Nikhildhuria01 <nikhilgg008@gmail.com>
2026-09-22 12:48:22 +0530

    dsds

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,7 +52,7 @@ def startup_check():
     print("==============================\n")
 
 
-ROOT = Path(__file__).resolve().parents[2]
+ROOT = Path(__file__).resolve().parents[1]
 MODEL_DIR = ROOT / "ml"
 
 RELATIONSHIP_MODEL_PATH = MODEL_DIR / "relationship_model.joblib"
