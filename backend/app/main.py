4a225849e9b34c595decde561b22d725149ed2cf
Nikhildhuria01 <nikhilgg008@gmail.com>
2026-09-22 12:45:20 +0530

    dsds

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,8 +52,8 @@ def startup_check():
     print("==============================\n")
 
 
-ROOT = Path(__file__).resolve().parents[2]
-MODEL_DIR = ROOT/"ml"
+ROOT = Path(__file__).resolve().parents[1]
+MODEL_DIR = ROOT / "ml"
 
 RELATIONSHIP_MODEL_PATH = MODEL_DIR / "relationship_model.joblib"
 # ANOMALY_MODEL_PATH = MODEL_DIR / "suspicious_pattern_model.joblib"
